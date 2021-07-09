4e786e599559f714690e8e19f68d2022b07a96dd
Alban MARGUET <alban.marguet@stormshield.eu>
2021-07-09 23:39:28 +0200

    Add dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("LOAD ENV");
+  require("dotenv").config();
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
