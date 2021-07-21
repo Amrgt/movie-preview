22b645bb4378a568eef6345402512b6aa1400780
Alban MARGUET <alban.marguet@stormshield.eu>
2021-07-21 22:50:55 +0200

    Fix warnings: no unused var

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
-  const [user, setUser] = useContext(Context);
+  const [setUser] = useContext(Context);
   const history = useHistory();
 
   const handleSubmit = async () => {
